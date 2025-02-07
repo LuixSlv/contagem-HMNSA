7e4d04e47db45512c7da5728c9b282039b8ea0d3
LuixSlv <155217216+LuixSlv@users.noreply.github.com>
2025-02-06 23:31:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { 
     const tabelaEsquerda = document.getElementById("tabela-esquerda");
     const tabelaDireita = document.getElementById("tabela-direita");
 
